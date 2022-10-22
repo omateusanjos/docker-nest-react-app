165f18bb659a36c4ad417cdcb14a51c67c00b5d1
mateusanjost <mateusanjosmgb@gmail.com>
2022-10-22 19:20:05 -0300

    add CSRF protection

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as csurf from 'csurf';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+
+  app.use(csurf());
+
 
   await app.listen(4000);
 }

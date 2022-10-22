0258a4e14362619a17bb318f99727a69053b8db6
mateusanjost <mateusanjosmgb@gmail.com>
2022-10-22 19:21:34 -0300

    add and use helmet basic protection

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import * as csurf from 'csurf';
-
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +13,7 @@ async function bootstrap() {
   });
 
   app.use(csurf());
-
+  app.use(helmet());
 
   await app.listen(4000);
 }

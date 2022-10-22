67951b39f2e47c8b9adf1fdfb8f0f0678c52bfc4
mateusanjost <mateusanjosmgb@gmail.com>
2022-10-22 19:43:19 -0300

    create doc config with OpenAPI and Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 import * as csurf from 'csurf';
 import helmet from 'helmet';
 
+const configOpenApi = new DocumentBuilder().setTitle('Hubla Back Challenge API')
+  .setDescription('Hubla Back Challenge API')
+  .setVersion('1.0')
+  .build();
+
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, configOpenApi);
+
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
   });
 
+  app.use(helmet({
+    contentSecurityPolicy: false,
+  }));
+
+
   app.use(csurf());
-  app.use(helmet());
 
   await app.listen(4000);
 }

44d10a092481ee77d2e961f21441071b98ed8d14
mateusanjost <mateusanjosmgb@gmail.com>
2022-10-28 13:27:39 -0300

    normalize - in init.sql

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE hubla
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'pt_BR.UTF8'
-    LC_CTYPE = 'pt_BR.UTF8'
+    LC_COLLATE = 'pt_BR.UTF-8'
+    LC_CTYPE = 'pt_BR.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;

486516cc7670d6e22142692f43621efbad4b9f88
mateusanjost <mateusanjosmgb@gmail.com>
2022-10-28 16:40:02 -0300

    fix: envs conflict with docker environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,8 @@ import { AuthService } from './auth/auth.service';
 
 const TypeORMConfig = TypeOrmModule.forRoot({
   type: 'postgres',
-  host: "database",
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT as unknown as number,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
